516517c876733edf0760afeca43e9941b02acc25
KvitselV <113378172+KvitselV@users.noreply.github.com>
2025-02-07 21:22:58 +0300

    Полный класс

--- 1/applicant.py
+++ 1/applicant.py
@@ -1,12 +1,31 @@
 import re
 import json
 class Applicant:
-  def __init__(self, id, name, address, phone, profession):
-    self._id = id
-    self._name = self.validate_value(name, "name", is_required=True, only_letters=True)
-    self._address = self.validate_value(address, "adress", is_required=True, only_letters=False)
-    self._phone = self.validate_value(phone, "phone", is_required=True, only_letters=False, regex=r'^\+\d{1,3}\d{3}\d{3}\d{4}$')
-    self._profession = self.validate_value(profession, "profession", is_required=True, only_letters=True)
+
+
+
+    def __init__(self, id, name, address, phone, profession):
+
+        self._id = id
+        self._name = self.validate_value(name, "name", is_required=True, only_letters=True)
+        self._address = self.validate_value(address, "adress", is_required=True, only_letters=False)
+        self._phone = self.validate_value(phone, "phone", is_required=True, only_letters=False, regex=r'^\+\d{1,3}\d{3}\d{3}\d{4}$')
+        self._profession = self.validate_value(profession, "profession", is_required=True, only_letters=True)
+
+    @staticmethod
+    def validate_value(value, field_name, is_required=True, only_letters=False, regex=None):
+
+        if is_required and not value.strip():
+            raise ValueError(f"{field_name} Не может быть пустой")
+
+        if only_letters and not value.replace(" ", "").isalpha():
+            raise ValueError(f"{field_name} Не может содержать цифр")
+
+        if regex and not re.match(regex, value):
+            raise ValueError(f"{field_name} Неверный формат: {regex}")
+
+        return value
+
 
     @classmethod
     def from_json(cls, json_str: str):
@@ -36,72 +55,60 @@ class Applicant:
             raise ValueError("Не корректный формат")
         except Exception as e:
             raise ValueError(f"Ошибка создания объекта: {str(e)}")
-    
-    @staticmethod
-    def validate_value(value, field_name, is_required=True, only_letters=False, regex=None):
 
-        if is_required and not value.strip():
-            raise ValueError(f"{field_name} Не может быть пустой")
+    def __repr__(self):
+        return f"Applicant(ID={self._id}, Имя='{self._name}')"
 
-        if only_letters and not value.replace(" ", "").isalpha():
-            raise ValueError(f"{field_name} Не может содержать цифр")
+    def __eq__(self, other):
+        if not isinstance(other, Applicant):
+            return False
+        return self._id == other._id
 
-        if regex and not re.match(regex, value):
-            raise ValueError(f"{field_name} Неверный формат: {regex}")
 
-        return value  
+    def get_id(self):
+        return self._id
 
-  def get_id(self):
-    return self._id
+    def set_id(self, id):
+        self._id = id
 
-  def set_id(self, id):
-    self._id = id
+    def get_name(self):
+        return self._name
 
-  def get_name(self):
-    return self._name
+    def set_name(self, name):
+        self._name = name
 
-  def set_name(self, name):
-    self._name = name
+    def get_address(self):
+        return self._address
 
-  def get_address(self):
-    return self._address
+    def set_address(self, address):
+        self._address = address
 
-  def set_address(self, address):
-    self._address = address
+    def set_phone(self, phone):
+        self._phone = phone
 
-  def get_phone(self):
-    return self._phone
+    def get_phone(self):
+        return self._phone
 
-  def set_phone(self, phone):
-    self._phone = phone
+    def get_profession(self):
+        return self._profession
 
-  def get_profession(self):
-    return self._profession
+    def set_profession(self, profession):
+       self._profession = profession
 
-  def set_profession(self, profession):
-    self._profession = profession
-
-  def __str__(self):
-    return f"Applicant(ID ={self._id}, Имя ='{self._name}', Адрес ='{self._address}', Телефон ='{self._phone}', Профессия ='{self._profession}')"
-
-  def __repr__(self):
-    return f"Applicant(ID={self._id}, Имя='{self._name}')"
-
-  def __eq__(self, other):
-    if not isinstance(other, Applicant):
-        return False
-    return self._id == other._id
+    def __str__(self):
+        return f"Applicant(ID ={self._id}, Имя ='{self._name}', Адрес ='{self._address}', Телефон ='{self._phone}', Профессия ='{self._profession}')"
 
 class ApplicantShort:
     def __init__(self, applicant):
         if not isinstance(applicant, Applicant):
-            raise TypeError("Коротка версия должна быть создана на основе полной")
-        self._id = applicant._id
-        self._name = applicant._name
-        self._phone = applicant._phone
+            raise TypeError("Короткая версия должна быть создана на основе полной")
+        self._id = applicant.get_id()
+        self._name = applicant.get_name()
+        self._phone = applicant.get_phone()
 
     def __str__(self):
         return f"ApplicantShort(ID={self._id}, Имя={self._name}, Телефон={self._phone})"
+
 
 json_data = '''
     {
